10b34dae2d9a6f24bc7e6f53b9b43e0bc52df7d0
Antoni Lunenko <antoni.lunenko@gmail.com>
2015-07-20 00:10:13 +0300

    init commit in gh-pages

---
+++ js/points.js
@@ -0,0 +1,96 @@
+var russia = [
+  {
+      'latitude': 59.9349065,
+      'longitude': 30.325761,
+      'title': 'St Petersburg',
+      'contentString1': '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">St Petersburg / Санкт-Петербург</h1>'+
+      '<div id="bodyContent">'+
+      '<p>visited June 22, 2009</p>'+
+      '</div>'+
+      '</div>'
+  }/*,
+  {
+      new google.maps.LatLng(60.012634, 29.721864),   /* Kronshtad 
+  },
+  {
+      new google.maps.LatLng(60.081296, 31.069050),   /* Doroga zizni 
+  },
+  {
+      new google.maps.LatLng(59.8866546,29.9087869),  /* Petergof 
+  },
+  {
+      new google.maps.LatLng(47.2610085,39.628),      /* Rostov-on-Don 
+  },
+  {
+      new google.maps.LatLng(47.0908465,39.4210618),  /* Azov 
+  },
+  {
+      new google.maps.LatLng(0,0),  /* Alania 
+  },
+  {
+      new google.maps.LatLng(43.443700, 41.744376),   /* Teberda 
+  },
+  {
+      new google.maps.LatLng(43.292954, 41.616660),   /* Dombai 
+  }*/
+];
+
+var poland = [
+      {
+            'latitude': -25.363882,
+            'longitude': 130.044922,
+            'title': 'my3',
+            'contentString1': '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">MY2</h1>'+
+            '<div id="bodyContent">'+
+            '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+            'sandstone rock formation in the southern part of the '+
+            'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+            'south west of the nearest large town, Alice Springs; 450&#160;km '+
+            '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+            'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+            'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+            'Aboriginal people of the area. It has many springs, waterholes, '+
+            'rock caves and ancient paintings. Uluru is listed as a World '+
+            'Heritage Site.</p>'+
+            '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+            'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+            '(last visited June 22, 2009).</p>'+
+            '</div>'+
+            '</div>'
+      },
+      {
+            'latitude': -25.363882,
+            'longitude': 120.044922,
+            'title': 'my4',
+            'contentString1': '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">MY44</h1>'+
+            '</div>'
+      }
+];
+
+var holland = [
+      {
+            'latitude': -25.363882,
+            'longitude': 110.044922,
+            'title': 'my4',
+            'contentString1': '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h1 id="firstHeading" class="firstHeading">MY544</h1>'+
+            '</div>'
+      }
+];
+
+var mapPoints = [
+      russia,
+      poland,
+      holland
+];
