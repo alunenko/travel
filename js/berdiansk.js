df7145f59272f4868ec2884e79b92d2a80c2e353
Antoni Lunenko <antoni.lunenko@gmail.com>
2015-08-15 23:58:05 +0300

    add berdiansk points

---
+++ js/berdiansk.js
@@ -0,0 +1,73 @@
+var berdianskData = [
+  {
+    "latitude": "46.749958",
+    "longitude": "36.788531",
+    "title": "",
+    "titleCyrillic": "Памятник Бычку кормильцу",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "46.748124",
+    "longitude": "36.790251",
+    "title": "",
+    "titleCyrillic": "Памятник Мальчику рыбаку",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "46.749846",
+    "longitude": "36.790142",
+    "title": "",
+    "titleCyrillic": "Памятник Сантехнику",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "46.720391",
+    "longitude": "36.823114",
+    "title": "",
+    "titleCyrillic": "Аквапарк Мыс доброй надежды",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "46.721630",
+    "longitude": "36.839529",
+    "title": "",
+    "titleCyrillic": "ДОЛ Морская республика",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "46.648538",
+    "longitude": "36.786690",
+    "title": "",
+    "titleCyrillic": "БО Золотой берег",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "46.636347",
+    "longitude": "36.761681",
+    "title": "Lighthouse",
+    "titleCyrillic": "Маяк нижний",
+    "date": "",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+];
+var berdiansk = [];
