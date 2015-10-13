437ad8a712f2c2e36b44b3dfc007aa8ae6855867
Antoni Lunenko <antoni.lunenko@gmail.com>
2015-10-14 01:01:44 +0300

    add points in svyatohirsk

--- js/svyatohirsk.js
+++ js/svyatohirsk.js
@@ -1,10 +1,90 @@
 var svyatohirskData = [
   {
-    "latitude": "49.04089",
-    "longitude": "37.5698134",
+    "latitude": "49.029540",
+    "longitude": "37.573414",
     "title": "",
-    "titleCyrillic": "Славяногорск",
-    "date": "",
+    "titleCyrillic": "пам. Артем",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.029089",
+    "longitude": "37.574229",
+    "title": "",
+    "titleCyrillic": "Мемориальный комплекс ВОВ II",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.027401",
+    "longitude": "37.565367",
+    "title": "",
+    "titleCyrillic": "Меловая скала, Свято-Никольский храм на меловой скале",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.029315",
+    "longitude": "37.569337",
+    "title": "",
+    "titleCyrillic": "Гостинница",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.028133",
+    "longitude": "37.567620",
+    "title": "",
+    "titleCyrillic": "Свято-Успенский собор",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.026107",
+    "longitude": "37.558908",
+    "title": "",
+    "titleCyrillic": "Скважины Лавры",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.028667",
+    "longitude": "37.538695",
+    "title": "",
+    "titleCyrillic": "600-летний дуб",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.028949",
+    "longitude": "37.536120",
+    "title": "",
+    "titleCyrillic": "Бездонное озеро",
+    "date": "6-15.01.2000",
+    "description": "",
+    "descriptionCyrillic": "",
+    "link": ""
+  },
+  {
+    "latitude": "49.047897",
+    "longitude": "37.548201",
+    "title": "",
+    "titleCyrillic": "Бывший санаторий Шахтёр",
+    "date": "6-15.01.2000",
     "description": "",
     "descriptionCyrillic": "",
     "link": ""
